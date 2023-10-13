f02c6aa5b828c05bc97399533c445e98ba63f689
Th3R3ality <63677818+Th3R3ality@users.noreply.github.com>
2023-10-13 14:25:28 +0200

    beginning of materials

---
+++ src/Material.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <d3d11.h>
+
+namespace Lapis {
+
+	class Material {
+	public:
+
+
+	private:
+		ID3D11VertexShader* vertexShader;
+		ID3D11PixelShader* vertexShader;
+
+
+	};
+}
