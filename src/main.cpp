7545878f9654288a923e62a4723063c8ec21e57e
Reality <63677818+Th3R3ality@users.noreply.github.com>
2023-12-28 11:27:43 +0100

    file refactoring done - i think

--- src/main.cpp
+++ src/main.cpp
@@ -36,7 +36,7 @@ LapisInstance engine;
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 // entry point
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
     
     // define window class
     WNDCLASSEX wc;
